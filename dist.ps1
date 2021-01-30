8c140e18a8458bfb6a38af6e968b0e8cf8c75929
Nick <NicholasMoser56@gmail.com>
2021-01-29 19:29:25 -0600

    Rename main to six_patches_of_pain

--- dist.ps1
+++ dist.ps1
@@ -1,8 +1,6 @@
-pyinstaller --onefile --icon=rinnegan.ico main.py
-Move-Item -Path dist/main.exe -Destination main.exe
-tar.exe -a -c -f Windows.zip main.exe data/xdelta3.exe
-Move-Item -Path main.exe -Destination dist/main.exe
-tar.exe -a -c -f Mac.zip main.py
-tar.exe -a -c -f Linux.zip main.py
-Remove-Item -Recurse -Force build
-Remove-Item -Recurse -Force dist
+pyinstaller --onefile --icon=rinnegan.ico six_patches_of_pain.py
+Move-Item -Path dist/six_patches_of_pain.exe -Destination six_patches_of_pain.exe
+tar.exe -a -c -f dist/windows.zip six_patches_of_pain.exe data/xdelta3.exe
+Move-Item -Path six_patches_of_pain.exe -Destination dist/six_patches_of_pain.exe
+tar.exe -a -c -f dist/mac.zip six_patches_of_pain.py
+tar.exe -a -c -f dist/linux.zip six_patches_of_pain.py
