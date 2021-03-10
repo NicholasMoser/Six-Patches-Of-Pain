fc645e6565417023f06fb71d7970d20c6c00ffd3
Nicholas Moser <NicholasMoser56@gmail.com>
2021-03-09 19:51:38 -0600

    0.1.0 release with new distribution script

--- dist.ps1
+++ dist.ps1
@@ -1,12 +1,27 @@
-$VERSION = "0.0.7"
+$VERSION = "0.1.0"
 
 # Recreate dist directory
 Remove-Item -Force -Recurse -Path dist -ErrorAction Ignore
 New-Item -ItemType Directory -Force -Path dist
 
-# Generate binaries
+# Generate Windows binary
+$Env:GOOS = "windows"; $Env:GOARCH = "amd64"
 go generate
 go build
 
-# Zip binaries
+# Zip Windows binary
 tar.exe -acf "dist/Six-Patches-Of-Pain-$VERSION-Windows.zip" Six-Patches-Of-Pain.exe data/xdelta3.exe
+
+# Generate Mac binary
+$Env:GOOS = "darwin"; $Env:GOARCH = "amd64"
+go build
+
+# Zip Mac binary
+tar.exe -acf "dist/Six-Patches-Of-Pain-$VERSION-Mac.zip" Six-Patches-Of-Pain
+
+# Generate Linux binary
+$Env:GOOS = "linux"; $Env:GOARCH = "amd64"
+go build
+
+# Zip Linux binary
+tar.exe -acf "dist/Six-Patches-Of-Pain-$VERSION-Linux.zip" Six-Patches-Of-Pain
